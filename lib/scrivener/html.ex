d78b6e64acf4a524f40505ca10dc962eb5a524c8
pau-riosa <jethro.riosa@gmail.com>
2024-05-11 06:13:37 +0800

    added moduledoc

--- lib/scrivener/html.ex
+++ lib/scrivener/html.ex
@@ -1,4 +1,5 @@
 defmodule Scrivener.HTML do
+  @moduledoc false
   use Phoenix.HTML
   @defaults [view_style: :bootstrap, action: :index, page_param: :page, hide_single: false]
   @view_styles [:bootstrap, :semantic, :foundation, :bootstrap_v4, :materialize, :bulma]
@@ -185,10 +186,8 @@ defmodule Scrivener.HTML do
          page_param: _page_param,
          params: _params
        )
-       when not (style in @view_styles) do
-    raise "Scrivener.HTML: View style #{inspect(style)} is not a valid view style. Please use one of #{
-            inspect(@view_styles)
-          }"
+       when style not in @view_styles do
+    raise "Scrivener.HTML: View style #{inspect(style)} is not a valid view style. Please use one of #{inspect(@view_styles)}"
   end
 
   # Bootstrap implementation
